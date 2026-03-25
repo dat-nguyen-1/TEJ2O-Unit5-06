55364c74a738b707d4712f627438a94ca0bd3d7a
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-25 13:20:03 -0400

    first commit

--- main.ts
+++ main.ts
@@ -5,4 +5,38 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// Initialize servos
+const servo1 = robotbit.Servos.S1
+robotbit.Servo(servo1, 0)
+
+// Reset display
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Handle button A press
+input.onButtonPressed(Button.A, function() {
+    // Turn servo to 0 degrees
+    robotbit.Servo(servo1, 0)
+
+    // Display degrees
+    basic.clearScreen()
+    basic.showString("0")
+    basic.showIcon(IconNames.SmallSquare)
+
+    // Reset display
+    basic.showIcon(IconNames.Happy)
+})
+
+// Handle button B press
+input.onButtonPressed(Button.B, function () {
+    // Turn servo to 180 degrees
+    robotbit.Servo(servo1, 180)
+
+    // Display degrees
+    basic.clearScreen()
+    basic.showString("180")
+    basic.showIcon(IconNames.SmallSquare)
+
+    // Reset display
+    basic.showIcon(IconNames.Happy)
+})
